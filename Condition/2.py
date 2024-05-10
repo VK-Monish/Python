805cf5dcb4fc3cdcd4793cad06c25b93c143562f
VK-MONISH <vkmonish2006@gmail.com>
2024-05-10 16:35:51 +0530

    deleted

--- Condition/2.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Enter the age"))
-if(a>=18):
-  print("you is eleigible for voteing")
-else:
-  print("you is not eligible for voteing")
--- Condition/3.py
+++
@@ -1,9 +0,0 @@
-mark = 80
-result =" "
-if mark < 35:
-	result = "failed"
-elif mark > 75:
-	result = "pass"
-else:
-	result = "passed"
-print(result)
